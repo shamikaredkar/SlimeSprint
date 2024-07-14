3507cad1ff6a91ada28dd27bf91370029043c557
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-14 14:06:53 -0400

    Added logic to let the player jump double times on long press and jump a short distance on single press

--- Player.js
+++ Player.js
@@ -1,10 +1,10 @@
 export default class Player {
-
     jumpPressed = false;
     jumpInProgress = false;
     falling = false;
     JUMP_SPEED = 0.6;
     GRAVITY = 0.4;
+
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio) {
         this.ctx = ctx;
         this.canvas = ctx.canvas;
@@ -21,6 +21,7 @@ export default class Player {
         // Position the player 65 pixels above the bottom of the canvas
         this.x = 10 * scaleRatio;
         this.y = this.canvas.height - this.height - (65 * scaleRatio);
+        this.yStandingPosition = this.y;
 
         // Load the idle image
         this.idle = new Image();
@@ -47,40 +48,61 @@ export default class Player {
             this.slimeRunImages[1] = slimeRunImage2;
         };
 
-        //Event Listeners for Keyboard
-        window.removeEventListener("keydown",this.keydown)
-        window.removeEventListener("keyup",this.keyup)
+        // Event Listeners for Keyboard
+        window.addEventListener("keydown", this.keydown);
+        window.addEventListener("keyup", this.keyup);
 
-        window.addEventListener("keydown",this.keydown)
-        window.addEventListener("keyup",this.keyup)
-
-        //Touch
-        window.removeEventListener("touchstart",this.touchstart);
-        window.removeEventListener("touchend",this.touchend);
-        window.addEventListener("touchstart",this.touchstart);
-        window.addEventListener("touchend",this.touchend);
+        // Touch
+        window.addEventListener("touchstart", this.touchstart);
+        window.addEventListener("touchend", this.touchend);
     }
 
     touchstart = () => {
         this.jumpInProgress = true;
     }
+
     touchend = () => {
-        this.jumpProgress = false;
+        this.jumpInProgress = false;
     }
 
     keydown = (event) => {
-        if(event.code === "Space"){
-            rhis.jumpPressed = true;
+        if (event.code === "Space") {
+            this.jumpPressed = true;
         }
     };
+
     keyup = (event) => {
-        if(event.code === "Space"){
-            rhis.jumpPressed = false;
+        if (event.code === "Space") {
+            this.jumpPressed = false;
         }
     };
 
     update(gameSpeed, frameTimeDelta) {
         this.run(gameSpeed, frameTimeDelta);
+        this.jump(frameTimeDelta);
+    }
+
+    jump(frameTimeDelta) {
+        if (this.jumpPressed) {
+            this.jumpInProgress = true;
+        }
+        if (this.jumpInProgress && !this.falling) {
+            if (this.y > this.canvas.height - this.minJumpHeight || (this.y > this.canvas.height - this.maxJumpHeight && this.jumpPressed)) {
+                this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
+            } else {
+                this.falling = true;
+            }
+        } else {
+            if (this.y < this.yStandingPosition) {
+                this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+                if (this.y + this.height > this.canvas.height) {
+                    this.y = this.yStandingPosition;
+                }
+            } else {
+                this.falling = false;
+                this.jumpInProgress = false;
+            }
+        }
     }
 
     run(gameSpeed, frameTimeDelta) {
@@ -102,6 +124,7 @@ export default class Player {
         }
     }
 }
+
 
 
 
